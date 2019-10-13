bb722113e935e29a50a91527c1fb655df980255e
NAPIER-MAIL\40204497 <40204497@live.napier.ac.uk>
2019-10-13 16:40:49 +0100

    Smoothed out moving between planets, made it not trippy

--- Unity Planet Project/Unity Planet Project/Assets/Scripts/FlyingCam.cs
+++ Unity Planet Project/Unity Planet Project/Assets/Scripts/FlyingCam.cs
@@ -6,41 +6,69 @@ using UnityEngine.UI;
 public class FlyingCam : MonoBehaviour
 {
 
+    float ScrollWheelChange;
+    Vector3 zoomBy = new Vector3(0f,0f,0f);
+
     public Text planetName;
 
     Transform currView; //Current transform info of the object the cameara should be facing at 
 
     public Transform[] cameraPoint;
 
-    float speedFactor = 0.2f;   //linear speed of camera
-    float angleFactor = 10f;    //Spherical interpolation of the camera compared to obkect it faces 
+    float speedFactor = 0.03f;   //linear speed of camera
+    float angleFactor = 0.03f;    //Spherical interpolation of the camera compared to obkect it faces 
 
     // Start is called before the first frame update
     void Start()
     {
         currView = transform;
 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, currView.position, speedFactor);  //starting point, end point, and how fast it will move in  etween them, Lerp = linear interpolation
-        transform.rotation = Quaternion.Slerp(transform.rotation, currView.rotation, angleFactor);
+        
 
-        planetName.rectTransform.anchoredPosition = RectTransformUtility.WorldToScreenPoint(Camera.main, currView.parent.GetChild(0).position);
+        //planetName.rectTransform.anchoredPosition = RectTransformUtility.WorldToScreenPoint(Camera.main, currView.parent.GetChild(0).position);
+
+         ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+
+        //zoom in and out
+        if (ScrollWheelChange != 0){                                            //If the scrollwheel has changed
+             float R = ScrollWheelChange * 15;                                   //The radius from current camera
+             float PosX = Camera.main.transform.eulerAngles.x + 90;              //Get up and down
+             float PosY = -1 * (Camera.main.transform.eulerAngles.y - 90);       //Get left to right
+             PosX = PosX / 180 * Mathf.PI;                                       //Convert from degrees to radians
+             PosY = PosY / 180 * Mathf.PI;                                       //^
+             float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);                    //Calculate new coords
+             float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);                    //^
+             float Y = R * Mathf.Cos(PosX);                                      //^
+             zoomBy = zoomBy + new Vector3(X,0,0);
+            //  float CamX = Camera.main.transform.position.x;                      //Get current camera postition for the offset
+            //  float CamY = Camera.main.transform.position.y;                      //^
+            //  float CamZ = Camera.main.transform.position.z;                      //^
+            //  Camera.main.transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);//Move the main camera
+         }
+
+         transform.position = Vector3.Lerp(transform.position, currView.position + zoomBy, speedFactor); //starting point, end point, and how fast it will move in  etween them, Lerp = linear interpolation
+         transform.rotation = Quaternion.Slerp(transform.rotation, currView.rotation, angleFactor);
     }
 
 
     public void SetNewPlanet(Transform p)
     {
+        zoomBy = new Vector3(0,0,0);    //Reset zoom values
         currView = p;
         planetName.text = currView.parent.GetChild(0).name; //camera is 2nd child of planet container, so we want the first child which is the planet
+
     }
 
     public void SetRandomPlanetView()
     {
         Transform temp = null;
+        zoomBy = new Vector3(0,0,0);    //Reset zoom values
 
         // Check if any planets can be highlighted
         if (cameraPoint.Length > 0)
