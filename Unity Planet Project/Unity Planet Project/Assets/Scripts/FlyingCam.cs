42060428150faf51a9590a95f4f5135036909631
NAPIER-MAIL\40204497 <40204497@live.napier.ac.uk>
2019-10-13 15:33:25 +0100

    Camera and UI from planet to planet now working, now to add particles and randomization script

--- Unity Planet Project/Unity Planet Project/Assets/Scripts/FlyingCam.cs
+++ Unity Planet Project/Unity Planet Project/Assets/Scripts/FlyingCam.cs
@@ -12,28 +12,38 @@ public class FlyingCam : MonoBehaviour
 
     public Transform[] cameraPoint;
 
-    float speedFactor = 0.5f;
-    float angleFactor = 10f;
+    float speedFactor = 0.2f;   //linear speed of camera
+    float angleFactor = 10f;    //Spherical interpolation of the camera compared to obkect it faces 
 
     // Start is called before the first frame update
     void Start()
     {
         currView = transform;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, currView.position, speedFactor);  //starting point, end point, and how fast it will move in  etween them, Lerp = linear interpolation
+        transform.rotation = Quaternion.Slerp(transform.rotation, currView.rotation, angleFactor);
+
+        planetName.rectTransform.anchoredPosition = RectTransformUtility.WorldToScreenPoint(Camera.main, currView.parent.GetChild(0).position);
+    }
+
+
+    public void SetNewPlanet(Transform p)
+    {
+        currView = p;
+        planetName.text = currView.parent.GetChild(0).name; //camera is 2nd child of planet container, so we want the first child which is the planet
     }
 
     public void SetRandomPlanetView()
     {
         Transform temp = null;
 
-    // Check if any planets can be highlighted
-        if (cameraPoint.Length>0)
+        // Check if any planets can be highlighted
+        if (cameraPoint.Length > 0)
         {
             temp = cameraPoint[Random.Range(0, cameraPoint.Length)];
         }
@@ -46,16 +56,16 @@ public class FlyingCam : MonoBehaviour
         }
         else
         {
-            {
-                currView = temp;
-                // Planet planet = currView.gameObject.GetComponent<Planet>();
 
-                // foreach (GameObject g in currView.parent)
-                // {
-                //     if ()
-                // }
-                planetName.text = currView.parent.GetChild(1).name;
-            }
+            currView = temp;
+            // Planet planet = currView.gameObject.GetComponent<Planet>();
+
+            // foreach (GameObject g in currView.parent)
+            // {
+            //     if ()
+            // }
+            planetName.text = currView.parent.GetChild(0).name; //camera is 2nd child of planet container, so we want the first child which is the planet
+
         }
     }
 
